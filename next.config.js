6471fd76bb45f92ec64e5d8f398bfde3474095de
Mohammad Golkar <m.golkar.dev@gmail.com>
2023-09-10 21:43:41 +0330

    chore: add images base url to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    domains: ["static.filmnet.ir"],
+  },
+};
+
+module.exports = nextConfig;
