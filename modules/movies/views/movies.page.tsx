ff6e21cf1ca2ce1191490724e0b7d1ebb74762c7
Mohammad Golkar <m.golkar.dev@gmail.com>
2023-09-10 21:52:28 +0330

    feat: add Movies page

---
+++ modules/movies/views/movies.page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MoviesModule } from "../movies.module";
+
+export const metadata = {
+  title: "Movies",
+};
+
+const Movies = () => {
+  return <MoviesModule />;
+};
+
+export default Movies;
