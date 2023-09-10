18bc741757f88325ee06a865a2d0e66995763184
Mohammad Golkar <m.golkar.dev@gmail.com>
2023-09-10 22:09:04 +0330

    chore: add redux provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-/** @format */
-
 "use client";
 
 import "shared/themes/globals.scss";
 import { MainLayout } from "shared/components/layout/main-layout.component";
+import { Provider } from "react-redux";
+import { appStore } from "shared/redux-state/app.store";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -12,7 +12,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <link rel="stylesheet" href="/css/fonts.css" />
       </head>
       <body>
-        <MainLayout>{children}</MainLayout>
+        <Provider store={appStore}>
+          <MainLayout>{children}</MainLayout>
+        </Provider>
       </body>
     </html>
   );
