9b3963cbbc8cfc9aef3340ad62796e2f757a776f
Mohammad Golkar <m.golkar.dev@gmail.com>
2023-09-10 21:48:34 +0330

    feat: add Movies base module

---
+++ app/movies/page.tsx
@@ -0,0 +1,1 @@
+export { default, metadata } from "modules/movies/views/movies.page";
---
+++ modules/movies/movies.module.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const MoviesModule = () => {
+  return (
+    <div>
+      Movies
+    </div>
+  );
+};
